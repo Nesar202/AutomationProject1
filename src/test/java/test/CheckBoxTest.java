7dd0bde5dec326418d9f49017d06e613f7230ba3
Nesar202 <nesar202@gmail.com>
2023-02-11 13:05:20 -0600

    Automation Project1

---
+++ src/test/java/test/CheckBoxTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import pages.CheckBox;
+import util.BrowserFactory;
+
+public class CheckBoxTest {
+
+	WebDriver driver;
+
+	@Test
+	public void UserShouldBeAbleCheckBox() {
+		driver = BrowserFactory.browserInit(); //
+
+		CheckBox checkBox = PageFactory.initElements(driver, CheckBox.class);
+		// CheckBox checkBox = new CheckBox
+		checkBox.CheckingToggle();
+		checkBox.validate_toggle_checkBox();
+
+	}
+
+}
